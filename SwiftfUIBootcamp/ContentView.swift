d4a618246b31756ecedd737a3d0559cfb7498974
Ronny Portillo <portilloronny6@gmail.com>
2023-10-15 13:32:06 +0500

    Initial Commit

---
+++ SwiftfUIBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfUIBootcamp
+//
+//  Created by Ronny David Portillo Machado on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfUIBootcamp/SwiftfUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfUIBootcampApp.swift
+//  SwiftfUIBootcamp
+//
+//  Created by Ronny David Portillo Machado on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
